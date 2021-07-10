1f6a11cb07c05e2234dd17626612207c0728568b
vittorio.malaspina@gmail.com <asd5589ijnmko00G>
2021-07-10 20:27:29 +0200

    .

--- src/Providers/LaravelHttpLoggerServiceProvider.php
+++ src/Providers/LaravelHttpLoggerServiceProvider.php
@@ -28,24 +28,5 @@ class LaravelHttpLoggerServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $tag = 'malviklab-laravel-http-logger';
-        $configFileName = sprintf('%s.php', $tag);
-
-        /*
-        $this->publishes([
-            sprintf('%s/../../config/%s', __DIR__, $configFileName) => config_path($configFileName),
-        ], sprintf('%s-config', $tag));
-        */
-
-        //$this->mergeConfigFrom(__DIR__.'/config/'.$this->_packageTag.'.php', $this->_packageTag);
-
-        /*
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadSeedsFrom();
-        */
-
-        //$this->publishFiles();
-
-
     }
 }
